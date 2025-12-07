ad49fdf328e111a952367cfa55002b38ca41ee49
KingBebo <KingBebo@users.noreply.github.com>
2025-12-06 20:07:23 -0600

    fixed calculations for Digital size

--- Digital size/Digi.js
+++ Digital size/Digi.js
@@ -13,59 +13,59 @@ Submit.onclick = () => {
             switch(To.value){
                 case "Bit": changedDigi = val; break;
                 case "Byte": changedDigi = val / 8; break;
-                case "Kilobyte": changedDigi = val / 8000; break;
-                case "Megabyte": changedDigi = val / 8e+6; break;
-                case "Gigabyte": changedDigi = val / 8e+9; break;
-                case "Terabyte": changedDigi = val / 8e+12; break;
+                case "Kilobyte": changedDigi = val / 8192; break;
+                case "Megabyte": changedDigi = val / (8 * 1024 * 1024); break;
+                case "Gigabyte": changedDigi = val / (8 * 1024 ** 3); break;
+                case "Terabyte": changedDigi = val / (8 * 1024 ** 4); break;
             }
             break;
         case "Byte":
             switch(To.value){
                 case "Bit": changedDigi = val * 8; break;
                 case "Byte": changedDigi = val; break;
-                case "Kilobyte": changedDigi = val / 1000; break;
-                case "Megabyte": changedDigi = val / 1e+6; break;
-                case "Gigabyte": changedDigi = val / 1e+9; break;
-                case "Terabyte": changedDigi = val / 1e+12; break;
+                case "Kilobyte": changedDigi = val / 1024; break;
+                case "Megabyte": changedDigi = val / (1024 * 1024); break;
+                case "Gigabyte": changedDigi = val / (1024 ** 3); break;
+                case "Terabyte": changedDigi = val / (1024 ** 4); break;
             }
             break;
         case "Kilobyte":
             switch(To.value){
-                case "Bit": changedDigi = val * 8000; break;
-                case "Byte": changedDigi = val * 1000; break;
+                case "Bit": changedDigi = val * 8192; break;
+                case "Byte": changedDigi = val * 1024; break;
                 case "Kilobyte": changedDigi = val; break;
-                case "Megabyte": changedDigi = val / 1000; break;
-                case "Gigabyte": changedDigi = val / 1e+6; break;
-                case "Terabyte": changedDigi = val / 1e+9; break; 
+                case "Megabyte": changedDigi = val / 1024; break;
+                case "Gigabyte": changedDigi = val / (1024 * 1024); break;
+                case "Terabyte": changedDigi = val / (1024 ** 3); break; 
             }
             break;
         case "Megabyte":
             switch(To.value){
-                case "Bit": changedDigi = val * 8e+6; break;
-                case "Byte": changedDigi = val * 1e+6; break;
-                case "Kilobyte": changedDigi = val * 1000; break;
+                case "Bit": changedDigi = val * (8 * 1024 * 1024); break;
+                case "Byte": changedDigi = val * (1024 * 1024); break;
+                case "Kilobyte": changedDigi = val * 1024; break;
                 case "Megabyte": changedDigi = val; break;
-                case "Gigabyte": changedDigi = val / 1000; break;
-                case "Terabyte": changedDigi = val / 1e+6; break;
+                case "Gigabyte": changedDigi = val / 1024; break;
+                case "Terabyte": changedDigi = val / (1024 * 1024); break;
             }
             break;
         case "Gigabyte":
             switch(To.value){
-                case "Bit": changedDigi = val * 8e+9; break;
-                case "Byte": changedDigi = val * 1e+9; break;
-                case "Kilobyte": changedDigi = val * 1e+6; break;
-                case "Megabyte": changedDigi = val * 1000; break;
+                case "Bit": changedDigi = val * (8 * 1024 ** 3); break;
+                case "Byte": changedDigi = val * (1024 ** 3); break;
+                case "Kilobyte": changedDigi = val * (1024 * 1024); break;
+                case "Megabyte": changedDigi = val * 1024; break;
                 case "Gigabyte": changedDigi = val; break;
-                case "Terabyte": changedDigi = val / 1000; break;
+                case "Terabyte": changedDigi = val / 1024; break;
             }
             break;
         case "Terabyte":
             switch(To.value){
-                case "Bit": changedDigi = val * 8e+12; break;
-                case "Byte": changedDigi = val * 1e+12; break;
-                case "Kilobyte": changedDigi = val * 1e+9; break;
-                case "Megabyte": changedDigi = val * 1e+6; break;
-                case "Gigabyte": changedDigi = val * 1000; break;
+                case "Bit": changedDigi = val * (8 * 1024 ** 4); break;
+                case "Byte": changedDigi = val * (1024 ** 4); break;
+                case "Kilobyte": changedDigi = val * (1024 ** 3); break;
+                case "Megabyte": changedDigi = val * (1024 * 1024); break;
+                case "Gigabyte": changedDigi = val * 1024; break;
                 case "Terabyte": changedDigi = val; break; 
             }    
             break;
